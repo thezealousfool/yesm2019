7aa9aeceda0929e544a83d1353515090280c7c09
Vivek Roy <thezealousfool@gmail.com>
2018-11-29 02:34:32 +0530

    event propagation stopped for Chrome

--- src/index.js
+++ src/index.js
@@ -8,23 +8,24 @@ function easeOutExpo(iter, start, end, duration, steepness) {
     return start + (end-start)*(1-Math.pow(2, (-1*steepness*iter/duration)))
 }
 
-function doScrolling(elementY, duration) { 
-  var startingY = window.pageYOffset || document.documentElement.scrollTop;
-  var iters = 0;
+function doScrolling(elementY, duration) {
+    var startingY = window.pageYOffset || document.documentElement.scrollTop;
+    var iters = 0;
 
-  window.requestAnimationFrame(function step() {
-    var toY = easeOutExpo(iters, startingY, elementY, duration, 15);
-    window.scrollTo(0, toY);
-    iters++;
-    if (iters < duration) {
-      window.requestAnimationFrame(step);
-    } else {
-        snapping=false;
-    }
-  })
+    window.requestAnimationFrame(function step() {
+        var toY = easeOutExpo(iters, startingY, elementY, duration, 15);
+        window.scrollTo(0, toY);
+        iters++;
+        if (iters < duration) {
+            window.requestAnimationFrame(step);
+        } else {
+            snapping=false;
+        }
+    })
 }
 
 window.addEventListener('scroll', function(e) {
+    e.preventDefault();
     var newTop = window.pageYOffset || document.documentElement.scrollTop;
     var delta = (newTop - currentTop);
     if (!snapping && Math.abs(delta) > 5) {
