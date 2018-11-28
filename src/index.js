dec8366b8244e4b272e677b3fa1ffe2b38c34f8c
Vivek Roy <thezealousfool@gmail.com>
2018-11-29 04:25:50 +0530

    Redefined animation using transforms. Touch not supported yet

--- src/index.js
+++ src/index.js
@@ -1,42 +1,93 @@
 import './main.css'
 import './index.css'
 
+var main = null;
+var offsets = [];
+var currentIndex = 0;
 var snapping = false;
-var currentTop = 0;
 
-function easeOutExpo(iter, start, end, duration, steepness) {
-    return start + (end-start)*(1-Math.pow(2, (-1*steepness*iter/duration)))
+function scroll_to() {
+    var str ='translateY(-'+offsets[currentIndex]+'px)';
+    main.style.transform = str;
+    window.setTimeout(() => snapping=false, 500);
 }
 
-function doScrolling(elementY, duration) {
-    var startingY = window.pageYOffset || document.documentElement.scrollTop;
-    var iters = 0;
-
-    window.requestAnimationFrame(function step() {
-        var toY = easeOutExpo(iters, startingY, elementY, duration, 15);
-        window.scrollTo(0, toY);
-        iters++;
-        if (iters < duration) {
-            window.requestAnimationFrame(step);
-        } else {
-            snapping=false;
-        }
-    })
-}
-
-window.addEventListener('scroll', function(e) {
-    e.preventDefault();
-    var newTop = window.pageYOffset || document.documentElement.scrollTop;
-    var delta = (newTop - currentTop);
-    if (!snapping && Math.abs(delta) > 5) {
-        snapping = true;
-        var direction = 1;
-        if (delta < 0) {
-            direction = -1;
-        }
-        var vh = window.innerHeight;
-        var elementY = (Math.floor(currentTop / vh) + direction)*vh;
-        currentTop = elementY;
-        doScrolling(elementY, 50);
+function scroll_to_first() {
+    if (currentIndex !== 0) {
+        currentIndex = 0;
+        scroll_to();
+    } else {
+        snapping = false;
     }
-})
+}
+
+function scroll_to_last() {
+    if (currentIndex !== offsets.length-1) {
+        currentIndex = offsets.length-1;
+        scroll_to();
+    } else {
+        snapping = false;
+    }
+}
+
+function scroll_down() {
+    if (currentIndex < offsets.length-1) {
+        ++currentIndex;
+        scroll_to();
+    } else {
+        snapping = false;
+    }
+}
+
+function scroll_up() {
+    if (currentIndex > 0) {
+        --currentIndex;
+        scroll_to();
+    } else {
+        snapping = false;
+    }
+}
+
+
+window.onload = () => {
+    main = document.getElementById("main");
+    var sections = document.getElementsByTagName('section');
+    for(var i = 0; i < sections.length; ++i) {
+        offsets.push(sections[i].offsetTop);
+    } 
+    main.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        if (!snapping) {
+            snapping = true;
+            if (e.deltaY > 0)
+                scroll_down();
+            else if (e.deltaY < 0)
+                scroll_up();
+            else
+                snapping = false;
+        }
+    });
+    window.addEventListener('keydown', (e) => {
+        if (!snapping) {
+            snapping = true;
+            if (e.which == 40 || e.which == 34) {
+                e.preventDefault();
+                scroll_down();
+            } else if (e.which == 38 || e.which == 33) {
+                e.preventDefault();
+                scroll_up();
+            } else if (e.which == 36) {
+                e.preventDefault();
+                scroll_to_first();
+            } else if (e.which == 35) {
+                e.preventDefault();
+                scroll_to_last();
+            } else {
+                snapping = false;
+                console.log(e.which);
+            }
+        } else {
+            e.preventDefault();
+        }
+    });
+}
