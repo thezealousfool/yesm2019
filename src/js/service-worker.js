8e3fd29d77eb0b9bca63ac3b51185b26a209b61d
Vivek Roy <thezealousfool@gmail.com>
2018-12-14 12:47:49 +0530

    Service worker deregistrer added. Don't know if it will work

---
+++ src/js/service-worker.js
@@ -0,0 +1,17 @@
+document.addEventListener('load', function() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(function(registrations) {
+            for(let registration of registrations) {
+                registration.unregister().then(function() {
+                    return self.clients.matchAll();
+                }).then(function(clients) {
+                    clients.forEach(client => {
+                        if (client.url && "navigate" in client){
+                            client.navigate(client.url));
+                        }
+                    });
+                });
+            }
+        });
+    }
+});
