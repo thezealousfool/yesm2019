0b0e74b7eda74e5469ff14a4d8648d0b5b6c47f3
Vivek Roy <thezealousfool@gmail.com>
2018-12-14 13:26:24 +0530

    Added log lines in the sw.
    Multiple commits around the same problem as I cannot test it before
    pushing it.

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,9 +1,12 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        console.log("Registrations", registrations);
         for(let registration of registrations) {
-            registration.unregister().then(function() {
+            registration.unregister().then(function(unreg_status) {
+                console.log(registration, unreg_status);
                 return self.clients.matchAll();
             }).then(function(clients) {
+                console.log(clients);
                 clients.forEach(client => {
                     if (client.url && "navigate" in client){
                         client.navigate(client.url);
@@ -12,4 +15,6 @@ if ('serviceWorker' in navigator) {
             });
         }
     });
+} else {
+    console.log("Service worker not found");
 }
